afe0e5bdd6a96cafcc7b452d5f4494491bb86b80
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-08-17 14:40:32 +0530

    marble: health: Rename to device-specific impl

--- device.mk
+++ device.mk
@@ -304,8 +304,7 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl-qti \
-    android.hardware.health@2.1-impl-qti.recovery \
+    android.hardware.health@2.1-impl-marble \
     android.hardware.health@2.1-service
 
 # HIDL
