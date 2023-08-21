510a48c15b0aff35a227ffb63f4572107bf6d2e8
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-08-21 13:21:18 +0530

    marble: parts: Introduce Dolby Atmos
    Based on existing dirac implementation and observing stock
    sound effects app and daxservice.
    
    Thanks to jhenrique09 for the hack from old dirac parts
    "Pause/play music stream to get effects applied".
    
    TODO: bring back misound (same as stock)
    
    Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>

--- parts/src/org/lineageos/settings/dolby/DolbyUtils.java
+++ parts/src/org/lineageos/settings/dolby/DolbyUtils.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.lineageos.settings.dolby;
+package org.pixelexperience.settings.dolby;
 
 import android.content.Context;
 import android.content.Intent;
