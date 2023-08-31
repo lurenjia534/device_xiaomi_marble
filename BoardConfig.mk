f4b2d23f8a041702df5f7e03077fcbd5094bb8ed
lurenjia534 <lurenjia534@outlook.com>
2023-08-31 09:49:44 +0800

    marble: Adapt to the pixel experience

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -254,7 +254,7 @@ DEVICE_MANIFEST_UKEE_FILES := \
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(CONFIGS_PATH)/vintf/vendor_framework_compatibility_matrix.xml \
     $(CONFIGS_PATH)/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/aosp/config/device_framework_matrix.xml
 ODM_MANIFEST_SKUS += marble
 ODM_MANIFEST_MARBLE_FILES := $(CONFIGS_PATH)/vintf/manifest_nfc.xml
 
