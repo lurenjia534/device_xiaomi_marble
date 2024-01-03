marble-development/android_device_xiaomi_marble_legacy

marble: parts: Refactor sensors
Change-Id: Ia18ceab3dbea29aa0fdcfc946a6fb81595c9ef94

parts/src/org/lineageos/settings/doze/DozeService.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2015 The CyanogenMod Project
- *               2017-2018 The LineageOS Project
+ *               2017-2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,6 +24,9 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
 import android.util.Log;
+
+import org.lineageos.settings.sensors.PickupSensor;
+import org.lineageos.settings.sensors.ProximitySensor;
 
 public class DozeService extends Service {
     private static final String TAG = "DozeService";

lurenjia534/device_xiaomi_marble

parts/src/org/pixelexperience/settings/doze/DozeService.java
@@ -25,6 +25,8 @@ import android.content.IntentFilter;
 import android.os.IBinder;
 import android.util.Log;
 
+
+import org.pixelexperience.settings.sensors.PickupSensor;
 import org.pixelexperience.settings.sensors.ProximitySensor;
 
 public class DozeService extends Service {