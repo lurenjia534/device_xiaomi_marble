a7891d86339e526aab4b2c859942bc57ee971d96
lurenjia534 <lurenjia534@outlook.com>
2024-01-04 08:37:37 +0800

    marble: parts: Import all parts from https://github.com/Nabu-upsidedowncake/device_xiaomi_marble

---
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *               2017-2020 The LineageOS Project
+ * Copyright (C) 2023 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
+import android.content.SharedPreferences;
+import android.os.SystemProperties;
+import androidx.preference.PreferenceManager;
+
+import org.lineageos.settings.camera.NfcCameraService;
+import org.lineageos.settings.display.ColorService;
+import org.lineageos.settings.dolby.DolbyUtils;
+import org.lineageos.settings.doze.AodBrightnessService;
+import org.lineageos.settings.doze.DozeUtils;
+import org.lineageos.settings.doze.PocketService;
+import org.lineageos.settings.display.ColorService;
+import org.lineageos.settings.thermal.ThermalUtils;
+import org.lineageos.settings.refreshrate.RefreshUtils;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+    private static final boolean DEBUG = false;
+    private static final String TAG = "XiaomiParts-BCR";
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        if (DEBUG) Log.d(TAG, "Received boot completed intent");
+        ThermalUtils.startService(context);
+
+        Log.i(TAG, "Boot completed");
+
+        // Dolby Atmos
+        DolbyUtils.getInstance(context);
+
+        // Doze
+        DozeUtils.checkDozeService(context);
+
+        // Pocket
+        PocketService.startService(context);
+
+        // DisplayFeature
+        ColorService.startService(context);
+
+        // NFC
+        NfcCameraService.startService(context);
+
+        // AOD
+        AodBrightnessService.startService(context);
+
+        // Per app refresh rate
+        RefreshUtils.startService(context);
+    }
+}
