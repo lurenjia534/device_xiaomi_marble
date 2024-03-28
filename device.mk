1bcc5b142bcba1cdad4a0c311fa3dfe197aa2592
lurenjia534 <lurenjia534@outlook.com>
2024-03-28 11:49:38 +0800

    marble: Opt for SCUDO and bypass jemalloc  - With recent jemalloc implementation in source, camera is dead, so lets opt in to scudo  - Reference: bananadroid/android_build@7186498
     bananadroid/android_build@7186498: https://github.com/bananadroid/android_build/commit/7186498b9bc609109e223187bbe7c5e59dc275c8

--- device.mk
+++ device.mk
@@ -533,6 +533,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/bin/init.kernel.post_boot.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.kernel.post_boot.sh
     $(LOCAL_PATH)/rootdir/bin/init.marble.perf.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.marble.perf.sh
 
+# Scudo
+PRODUCT_USE_SCUDO := true
+
 # Secure element
 PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.2.vendor
